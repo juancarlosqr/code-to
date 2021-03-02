5935db19df0c90bf528d44d239621ec118ea8b2c
Juan Carlos Quintero <jcquintero.ce@gmail.com>
2021-03-02 06:10:27 +0100

    Setup typescript

---
+++ pages/index.tsx
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Next.JS + Fireship</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>
+          Next.JS + Fireship
+        </h1>
+        </main>
+    </div>
+  )
+}
