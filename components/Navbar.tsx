d8280010b61b6ef4a96b19e170ca184bc8b2ccc6
Juan Carlos Quintero <jcquintero.ce@gmail.com>
2021-03-03 04:50:07 +0100

    Add Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,5 +1,46 @@
+import Link from 'next/link';
+
 const Navbar = () => {
-  return <section>Navbar</section>;
+  const user = null;
+  const username = null;
+
+  return (
+    <nav className="navbar">
+      <ul>
+        <li>
+          <Link href="/">
+            <button className="btn-logo">CODE</button>
+          </Link>
+        </li>
+
+        {username ? (
+          <>
+            <li className="push-left">
+              <Link href="/">
+                <button>Log out</button>
+              </Link>
+            </li>
+            <li>
+              <Link href="/admin">
+                <button className="btn-blue">Write Posts</button>
+              </Link>
+            </li>
+            <li>
+              <Link href={`/${username}`}>
+                <img src={user?.photoURL} alt="" />
+              </Link>
+            </li>
+          </>
+        ) : (
+          <li>
+            <Link href="/enter">
+              <button className="btn-blue">Log in</button>
+            </Link>
+          </li>
+        )}
+      </ul>
+    </nav>
+  );
 };
 
 export default Navbar;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,14 @@
+import Navbar from '@/components/Navbar';
 import '../styles/globals.css';
 
 function CodeApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Navbar />
+      <Component {...pageProps} />
+      );
+    </>
+  );
 }
 
 export default CodeApp;
