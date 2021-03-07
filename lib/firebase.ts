3b21c0fbb5bed8c9a0b6cea110d01eb3e8d82baf
Juan Carlos Quintero <jcquintero.ce@gmail.com>
2021-03-07 07:28:50 +0100

    Add google sign in button

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -17,5 +17,6 @@ if (!firebase.apps.length) {
 }
 
 export const auth = firebase.auth();
+export const googleOAuthProvider = new firebase.auth.GoogleAuthProvider();
 export const firestore = firebase.firestore();
 export const storage = firebase.storage();
--- pages/enter.tsx
+++ pages/enter.tsx
@@ -1,7 +1,46 @@
+import { auth, googleOAuthProvider } from '@/lib/firebase';
+
+const UsernameForm = () => {
+  return <section>Username</section>;
+};
+
+const SignInButton = () => {
+  const signInWithGoogle = async () => {
+    await auth.signInWithPopup(googleOAuthProvider);
+  };
+
+  return (
+    <button className="btn-google" onClick={signInWithGoogle}>
+      <img src="/google.png" alt="Google Sign In" /> Sign in with Google
+    </button>
+  );
+};
+
+const SignOutButton = () => {
+  return (
+    <button className="btn" onClick={() => auth.signOut()}>
+      Sign Out
+    </button>
+  );
+};
+
 const EnterPage = () => {
+  const user = false;
+  const username = false;
+
   return (
     <main>
       <h1>Enter</h1>
+
+      {user ? (
+        username ? (
+          <SignOutButton />
+        ) : (
+          <UsernameForm />
+        )
+      ) : (
+        <SignInButton />
+      )}
     </main>
   );
 };
