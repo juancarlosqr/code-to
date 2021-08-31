f293d9c408f31a18ab8b8efab568669566da9c59
Juan Carlos Quintero <jcquintero.ce@gmail.com>
2021-08-31 08:25:41 +0200

    Add custom 404

--- pages/[username]/index.tsx
+++ pages/[username]/index.tsx
@@ -7,6 +7,13 @@ export async function getServerSideProps({ query }) {
 
   const userDoc = await getUserWithUsername(username);
 
+  // If no user, short circuit to 404 page
+  if (!userDoc) {
+    return {
+      notFound: true,
+    };
+  }
+
   // JSON serializable data
   let user = null;
   let posts = null;
